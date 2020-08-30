906f064f916e07c960fd8b870a84278c6599d80a
josue487 <josue487@gmail.com>
2020-08-30 18:39:39 -0300

    usuario con encapsulado

--- ClasesBase/Usuario.cs
+++ ClasesBase/Usuario.cs
@@ -4,7 +4,16 @@
     {
         private int usu_ID;
         private string usu_NombreUsuario;
+        private string usu_Contraseña;
+        private string usu_ApellidoNombre;
+        private string rol_Codigo;
 
-        public int Usu_Id { get => usu_ID; set => usu_ID = value; }
+
+
+        public int Usu_ID { get => usu_ID; set => usu_ID = value; }
+        public string Usu_NombreUsuario { get => usu_NombreUsuario; set => usu_NombreUsuario = value; }
+        public string Usu_Contraseña { get => usu_Contraseña; set => usu_Contraseña = value; }
+        public string Usu_ApellidoNombre { get => usu_ApellidoNombre; set => usu_ApellidoNombre = value; }
+        public string Rol_Codigo { get => rol_Codigo; set => rol_Codigo = value; }
     }
 }
