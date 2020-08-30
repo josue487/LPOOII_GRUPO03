7638ff2a8d0353e6af852ef0de75d6f396773a12
josue487 <josue487@gmail.com>
2020-08-30 18:41:37 -0300

    rol

--- ClasesBase/Rol.cs
+++ ClasesBase/Rol.cs
@@ -7,5 +7,10 @@ namespace ClasesBase
 {
     public class Rol
     {
+        private string rol_Codigo;
+        private string rol_Descripcion;
+
+        public string Rol_Codigo { get => rol_Codigo; set => rol_Codigo = value; }
+        public string Rol_Descripcion { get => rol_Descripcion; set => rol_Descripcion = value; }
     }
 }
